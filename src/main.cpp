02f85f2b58b0a95f329695a244d90c49643915a2
Vasek <vaclav.burda@centrum.cz>
2023-06-01 18:43:12 +0200

    funguje ovladac

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "BluetoothSerial.h"
 #include "robotka.h"
 #include <thread>
+#include "SmartServoBus.hpp"
+#include "arm_commands.h"
 
 #define DELAY 50
 #define DELAY_BLINK 250
 #define LED_COUNT 8
-
+int plus_or_minus = 1;
 // podle potreby proved kalibraci 
 // vyrobte klepeta a nahradni baterky 
 // sezeneme krabici 
@@ -49,6 +51,28 @@ void setup() {
     fmt::print("{}'s SokolSus '{}' with {} mV started!\n", cfg.owner, cfg.name, rkBatteryVoltageMv());
     rkLedYellow(true); // robot je připraven
 
+    rkSetup(cfg);
+    servoBus.begin(4, UART_NUM_2, GPIO_NUM_27);
+    servoBus.set
+
+
+    servoBus.setAutoStop(0, true);
+    printf("Start\n");
+    
+
+    while (true)
+    {
+        // delay(1000);
+        // servoBus.set(254, Angle::deg(110), 200.f, 1.f);
+        // delay(2000);
+        for(int i = 0; i < 255; i++) {
+            // printf("S %d = %d\n", servoBus.getId(i));
+            servoBus.set(i; Angle::deg(110), 200.f, 1.f)
+            delay(100);
+        }
+    }
+    
+
     while(true) {
         if ( read_joystick() )
             {
@@ -56,11 +80,28 @@ void setup() {
                 //axis_0 = axis_0*axis_0*axis_0;
                 float axis_2 = (abs(axis[2]) < 10) ? 0 : -axis[2] /128.0; 
                 // axis_1 = axis_1*axis_1*axis_1;
-                int levy_m = ((axis_2 * -1) - (axis_0 /2 )) * speed_coef;  // hodnota pro levy motor
-                int pravy_m = ((axis_2 * -1) + (axis_0 /2 )) * speed_coef; // hodnota pro pravy motor
 
+
+                int levy_m = ((axis_2 * -plus_or_minus) - (axis_0 /2 )) * speed_coef;  // hodnota pro levy motor
+                int pravy_m = ((axis_2 * -plus_or_minus) + (axis_0 /2 )) * speed_coef; // hodnota pro pravy motor
+
+                if(levy_m + pravy_m > 160 || levy_m + pravy_m < -160)
+                {
+                    levy_m = ((axis_2 * -plus_or_minus) - (axis_0 /4 )) * speed_coef;
+                    pravy_m = ((axis_2 * -plus_or_minus) + (axis_0 /4 )) * speed_coef;
+                }
+
+        if (btn[0] == true)
+                { 
+                    plus_or_minus = 1;
+            }
                 Serial.println(levy_m);
                 
+        if (btn[0] == false)
+            { 
+                plus_or_minus = -1;
+            }
+                Serial.println(levy_m);
 
                 if (BTworks) {
                     SerialBT.print(levy_m); SerialBT.print(" "); SerialBT.println(pravy_m);
